d222908b7771086155b62179705a35c404143942
Vincent <592196977@qq.com>
2019-08-24 19:44:44 +0800

    Refactor home page view to use a template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,16 +1,11 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
+
 # Create your tests here.
-
-class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        # 解析url，找到相应的试图函数
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
 class HomePageTest(TestCase):
 
@@ -27,7 +22,22 @@ class HomePageTest(TestCase):
         # 提取响应中的content，并且把原始字节转换诚html字符串
         html = response.content.decode('utf-8')
 
+        self.assertTrue(html.strip().startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
+    
+    def test_home_page_return_correct_html_v2(self):
+        response = self.client.get('/')
+        html = response.content.decode('utf-8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
 
+        self.assertTemplateUsed(response, 'home.html')
