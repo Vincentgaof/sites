59c82ae3a2a5f1b5b8f972a3c804a17b61ebab33
Vincent <592196977@qq.com>
2019-08-25 23:37:36 +0800

    test template variable

--- superlists/function_test03.py
+++ superlists/function_test03.py
@@ -17,6 +17,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊丽丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的我首页
@@ -42,18 +47,22 @@ class NewVisitorTest(unittest.TestCase):
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了 "1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        # 这里很重要休眠以下，才能查看到response
+        time.sleep(3)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'False is not true: New to-do item did not appear in table'
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了 “Use paecock feathers to make a fly”
         # 伊丽丝做事很有条理
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+        # 页面再次更新，她的清单中显示了这了两个待办事项
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Finish the test')
 
 if __name__ == '__main__':
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
 # Create your tests here.
 
@@ -25,13 +26,6 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
-
-    def test_home_page_return_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
     
     def test_home_page_return_correct_html_v2(self):
         response = self.client.get('/')
@@ -41,3 +35,27 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
 
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
+    
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
